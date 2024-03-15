98b08891c36fdebc21a7b31a1b00385e6cb65176
ushieru <uzielcocolan@gmail.com>
2024-03-15 12:07:43 -0600

    fix: add Method not implemented error

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -1,28 +1,29 @@
 import { Router } from "express"
 import asyncHandler from 'express-async-handler'
 import { UserRepository } from "../repository/UserRepository"
+import { DomainError } from '../model/errors/DomainError'
 
 export const UserRouter = (userRepository: UserRepository) => {
     const router = Router()
 
     router.get('/', asyncHandler(async (req, res) => {
-
+        throw new DomainError(500, "Method not implemented.")
     }))
 
     router.get('/:id', asyncHandler(async (req, res) => {
-
+        throw new DomainError(500, "Method not implemented.")
     }))
 
     router.post('/', asyncHandler(async (req, res) => {
-
+        throw new DomainError(500, "Method not implemented.")
     }))
 
     router.put('/:id', asyncHandler(async (req, res) => {
-
+        throw new DomainError(500, "Method not implemented.")
     }))
 
     router.delete('/:id', asyncHandler(async (req, res) => {
-
+        throw new DomainError(500, "Method not implemented.")
     }))
 
     return router
